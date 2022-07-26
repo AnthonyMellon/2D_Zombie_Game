0ea0c5f8ee272d3c117d7d5d2bb30b4cad90c58b
Anthony <anthonymellon@yahoo.com>
2022-07-26 16:38:16 +1200

    player moves using joystick

---
+++ 2D_Zombie_Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [Header("Joysticks")]
+    public Joystick joystickMovement;
+    public Joystick joystickAim;
+    [Space]
+    [Header("Movement")]
+    public float moveSpeed = 10f;
+
+    private float horizontalMovement = 0f;
+    private float verticalMovement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = joystickMovement.Horizontal * moveSpeed;
+        verticalMovement = joystickMovement.Vertical * moveSpeed;
+
+        transform.Translate(new Vector3(horizontalMovement, verticalMovement, 0));
+    }
+}
