7ea501d06161ca7807ce16b2d8fa9a4bc7c24123
Anthony <anthonymellon@yahoo.com>
2022-07-27 23:39:28 +1200

    player can manually reload. reloads cant stack. renamed reloadRound to cycleRound, moved ammoInMag reduction into cycleRound

--- 2D_Zombie_Game/Assets/Scripts/Weapons/Weapon.cs
+++ 2D_Zombie_Game/Assets/Scripts/Weapons/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Joystick weaponJoystick;
     [Range(0, 1)]
     [SerializeField] private float joystickDeadZone;
+    private const KeyCode RELOAD_KEY = KeyCode.R;
 
     [Header("Weapon Stats")]    
     [SerializeField] private float damage;
@@ -16,11 +17,12 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float fireRate;
     [SerializeField] private float magSize;
     [Tooltip("How many seconds it takes to reload")]
-    [SerializeField] private float reloadSpeed;
+    [SerializeField] private float reloadSpeed;    
 
     [Header("Debug")]
     [SerializeField] private float ammoInMag;
     [SerializeField] private bool roundLoaded = true;
+    [SerializeField] private bool reloading = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,36 +33,52 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Shoot when the joystick is moved outside the deadzone, and if the player as ammo to shoot
         if(Mathf.Abs(weaponJoystick.Horizontal) > joystickDeadZone || Mathf.Abs(weaponJoystick.Vertical) > joystickDeadZone)
         {
             if (ammoInMag > 0 && roundLoaded) shoot();
+        } 
+        
+        //Manual reload
+        if(Input.GetKeyDown(RELOAD_KEY))
+        {
+            if(!reloading && ammoInMag < magSize)
+            {
+                StartCoroutine(reloadMag());
+            }
         }
     }    
 
     private void shoot()
     {
-        ammoInMag -= 1;
-        roundLoaded = false;
+        StartCoroutine(cycleRound());
 
-        StartCoroutine(reloadRound());
-
+        //Automatic reload
         if (ammoInMag == 0)
         {
             StartCoroutine(reloadMag());
         }
     }
 
-    //Used after every bullet is shot
-    private IEnumerator reloadRound()
+    //Cycles the round in the weapon
+    private IEnumerator cycleRound()
     {
+        ammoInMag -= 1;
+        roundLoaded = false;
         yield return new WaitForSeconds(1 / fireRate);
         roundLoaded = true;
     }
 
     //Used after the entier mag is spent
     private IEnumerator reloadMag()
-    {
-        yield return new WaitForSeconds(reloadSpeed);
-        ammoInMag = magSize;
+    {        
+        if(!reloading)
+        {
+            reloading = true;
+            yield return new WaitForSeconds(reloadSpeed);
+            ammoInMag = magSize;
+            reloading = false;
+        }        
+       
     }
 }
