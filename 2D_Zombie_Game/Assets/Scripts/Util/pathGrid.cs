fb18453843610c5f00a2fff241ca622f578cba16
Anthony <anthonymellon@yahoo.com>
2022-08-09 00:12:42 +1200

    create pathGrid class

---
+++ 2D_Zombie_Game/Assets/Scripts/Util/pathGrid.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathGrid
+{
+    private int numCellsX;
+    private int numCellsY;
+    private float cellSize;
+    private Vector2 origin;
+
+    private pathCell[,] cells;
+
+    public pathGrid(int numCellsX, int numCellsY, int cellSize, Vector2 origin)
+    {
+        this.numCellsX = numCellsX;
+        this.numCellsY = numCellsY;
+        this.cellSize = cellSize;
+        this.origin = origin;
+    }
+
+    public void setup()
+    {
+        for(int x = 0; x < numCellsX; x++)
+        {
+            for(int y = 0; y < numCellsY; y++)
+            {
+                float worldPosX = origin.x + (cellSize * x);
+                float worldPosY = origin.y + (cellSize * y);
+
+                cells[x, y] = new pathCell();
+            }
+        }
+    }
+}
