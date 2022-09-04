e08e1929f5ddb141e6185e7b29b64d1ef5c0be3f
Anthony <anthonymellon@yahoo.com>
2022-09-05 01:13:51 +1200

    functional and styalised health bar

---
+++ 2D_Zombie_Game/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class HealthBar : Slider
+{
+    [SerializeField]
+    private Entity_SO player;
+    [SerializeField]
+    public Gradient healthColours;
+    [SerializeField]
+    private float lerpSpeed;
+
+    private void FixedUpdate()
+    {
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        if (player)
+        {
+            float val = Mathf.Lerp(m_Value, player.currentHealth / player.maxHealth, Time.deltaTime * lerpSpeed);
+            fillRect.transform.GetComponent<Image>().color = healthColours.Evaluate(val);
+            Set(val, true);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        if (!Application.isPlaying)
+        {
+            UpdateBar();
+            UnityEditor.EditorApplication.QueuePlayerLoopUpdate();            
+            UnityEditor.SceneView.RepaintAll();
+        }
+#endif
+    }
+
+
+}
---
+++ 2D_Zombie_Game/Assets/Scripts/UI/HealthBarEditor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(HealthBar))]
+public class HealthBarEditor : Editor
+{
+    
+}
